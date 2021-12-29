28dd9592ae7343eadd67ff951cac444d2964c15e
nacsha <nacshapg@gmail.com>
2021-12-30 02:40:21 +0900

    improve

--- perl/qsort.pl
+++ perl/qsort.pl
@@ -1,6 +1,5 @@
 use strict;
 use warnings;
-use Data::Dumper;
 
 main();
 
@@ -15,13 +14,14 @@ sub main {
 
 sub quick_sort {
     my ($arr) = @_;
-    return $arr if (scalar(@$arr) == 1);
+    return $arr if (scalar(@$arr) == 0);
 
-    my ($left, $right) = partition($arr);
+    my ($left, $right, $pivot_arr) = partition($arr);
     $left = quick_sort($left);
     $right = quick_sort($right);
     my @sorted_arr;
     push @sorted_arr, @$left;
+    push @sorted_arr, @$pivot_arr;
     push @sorted_arr, @$right;
     return \@sorted_arr;
 }
@@ -30,38 +30,24 @@ sub partition {
     my ($arr) = @_;
 
     my $pivot = $arr->[0];
-    my ($left, $right) = _part_partition($arr, $pivot);
-    if (scalar(@$right) == 0) {
-        # @$rightに何も入らないケースに対応
-        # leftには必ずなにか入る
-        $left = [];
-        $pivot = $arr->[1];
-        ($left, $right) = _part_partition($arr, $pivot);
-    }
-    return ($left, $right);
+    my ($left, $right, $pivot_arr) = _part_partition($arr, $pivot);
+    return ($left, $right, $pivot_arr);
 }
 
 sub _part_partition {
     my ($arr, $pivot) = @_;
     my @left;
     my @right;
+    my @pivot;
 
-    my $counter = 0;
     foreach my $elem (@$arr) {
         if ($elem < $pivot) {
             push @left, $elem;
         } elsif ($elem > $pivot) {
             push @right, $elem;
         } else {
-            # 1回目はleft, 2回目以降はrightに入れる
-            # 2回目というのは、同じ数字が複数回出たとき用の対応
-            $counter++;
-            if ($counter >= 2) {
-                push @right, $elem;
-            } else {
-                push @left, $elem;
-            }
+            push @pivot, $pivot;
         }
     }
-    return (\@left, \@right);
+    return (\@left, \@right, \@pivot);
 }
